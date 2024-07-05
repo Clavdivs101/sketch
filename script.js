53580df37d4d1f9b732ef8e48d7e47cfe6f78c7e
odinstudent <166247290+Clavdivs101@users.noreply.github.com>
2024-07-05 11:05:06 +0300

    index.html

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+const gridContainer = document.getElementById('gridContainer');
+const generateBtn = document.querySelector('#generate')
+
+
+
+ function getValue(){
+    let input = document.getElementById('myInput');
+
+    let value = input.value;
+    return value;
+ }
+
+
+function generateGrid(){
+    gridContainer.replaceChildren();
+for (let i = 1; i <= getValue(); i++){
+    for(let j = 1; j <= getValue(); j++)
+        {
+    let gridItem = document.createElement("div");
+    gridItem.className = 'grid-item'
+    gridContainer.appendChild(gridItem);
+    gridContainer.style.width = `${(getValue() * (10 + 2))}px`
+    gridContainer.style.height = `${(getValue() * (10 + 2))}px`
+
+
+
+    gridItem.addEventListener("mouseover", () => {
+        gridItem.style.background = 'blue';
+    })
+
+    }
+}
+}
+
+
+
+generateBtn.addEventListener("click", () => {
+    generateGrid(getValue());
+})
