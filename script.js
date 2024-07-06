ae5d623b51026272a2bf3e448fdfc7d44256f744
odinstudent <166247290+Clavdivs101@users.noreply.github.com>
2024-07-06 17:24:38 +0300

    implemented random colors plus a few extra buttons

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 
 
 const gridContainer = document.getElementById('gridContainer');
-const generateBtn = document.querySelector('#generate')
+const generateBtn = document.querySelector('#generate');
+const blackBtn = document.querySelector('#blackBtn');
+const chaosBtn = document.querySelector('#chaosBtn');
+const resetBtn = document.querySelector('#resetBtn');
+const eraserBtn = document.querySelector('#eraserBtn');
+
+document.getElementById("blackBtn").disabled = true;
+document.getElementById("chaosBtn").disabled = true;
+document.getElementById("resetBtn").disabled = true;
+document.getElementById("eraserBtn").disabled = true;
 
 
 
  function getValue(){
     let input = document.getElementById('myInput');
-
     let value = input.value;
     return value;
  }
+
+ function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+
+
+
+
+
 
 
 function generateGrid(){
@@ -25,21 +47,44 @@ for (let i = 1; i <= getValue(); i++){
     gridContainer.style.height = `${(getValue() * (10 + 2))}px`
 
 
+    blackBtn.addEventListener('click', () => {
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.background = 'black';
+        });
+    });
+    chaosBtn.addEventListener('click', () => {
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.background = generateRandomColor();
+        });
+    });
+    resetBtn.addEventListener('click', () => {
+        gridItem.style.background = 'white';
+    });
+    eraserBtn.addEventListener('click', () => {
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.style.background = 'white';
+        });
+    });
 
-    gridItem.addEventListener("mouseover", () => {
-        gridItem.style.background = 'blue';
-    })
+
+
+
+
 
     }
 }
 }
 
-
-
 generateBtn.addEventListener("click", () => {
-    if(getValue() > 100){
-        alert('The limit is 100')
-    }else if (getValue() <= 100){
+
+
+    if(getValue() > 70 || getValue() < 1){
+        alert('Enter a value between 1 and 70')
+    }else if (getValue() <= 70 && getValue() >= 1){
+document.getElementById("blackBtn").disabled = false;
+document.getElementById("chaosBtn").disabled = false;
+document.getElementById("resetBtn").disabled = false;
+document.getElementById("eraserBtn").disabled = false;
     generateGrid(getValue());
         }
     }
